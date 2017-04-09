f5623a6de1adfea118967ce04f4f5fba81308037
SergKolo <1047481448@qq.com>
2017-04-09 13:10:24 -0600

    program for integrating polynomial

---
+++ integrate_polynomial.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+
+    int degree;
+    printf(">>> Enter polynomial degree:\n");
+    scanf("%d",&degree);
+
+    printf(">>> NOTE: Polynomial is read from lowest power to highest:\n");
+    printf("For example: A*x^0 + B*x^1 + C*x^2...\n");
+
+    double *coefficients;
+    coefficients = malloc(degree * sizeof(double));
+
+    for(int i=0; i<=degree; i++){
+        printf(">>> Enter coefficient #%d:\n",i);
+        scanf("%lf",&coefficients[i]);
+    }
+
+    printf(">>> Enter limits of integration\n");
+
+    double a,b;
+    printf(">>> a:");
+    scanf("%lf",&a);
+    printf(">>> b:");
+    scanf("%lf",&b);
+
+    // step size h
+    double h;
+    h = (b-a)/10;
+
+    /*
+        We need to iterate over all trapezoids, calculating
+        F(x) at each edge.
+     */
+
+    double x,y,integral;
+    for(int i=1;i<=9;i++){
+
+       x = a + i*h;
+       printf("ENDPOINT:%lf\t",x);
+       // value of polynomial is sum of all
+       // producs of coefficients and x^j 
+       for(int j=0;j<=degree;j++){
+           y += coefficients[j] * pow(x,j);
+       } 
+       printf("F(x):%lf\n",y);
+       integral += y;
+       y=0;
+    }
+
+    /*
+     Now we need to compute F(x) at the edges
+     and add the remaining value to the integral
+     */
+    double y_a,y_b;
+    
+    for(int j=0;j<=degree;j++){
+        y_a += coefficients[j] * pow(a,j);
+        y_b += coefficients[j] * pow(b,j);
+    } 
+    printf("ENDPOINT:%lf\tF(x):%lf\n",a,y_a);
+    printf("ENDPOINT:%lf\tF(x):%lf\n",b,y_b);
+    integral += (y_a + y_b)/2.0;
+
+    printf(">>> Integral using trapezoidal rule of this polynomial is:\n");
+    printf("%lf\n",integral*h);
+
+    return 0;
+}
